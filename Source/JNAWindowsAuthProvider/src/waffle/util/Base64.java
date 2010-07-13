58ddc6773571ee47bfadbc25710e793b5606a1b3
dblock <none@none.com>
2010-07-13 10:30:30 +0000

    Found original copyright owner for the Base64 class (JCIFS).

--- Source/JNAWindowsAuthProvider/src/waffle/util/Base64.java
+++ Source/JNAWindowsAuthProvider/src/waffle/util/Base64.java
@@ -1,17 +1,28 @@
-/*
- * Copyright (c) Application Security Inc., 2010
- * All Rights Reserved
- * Eclipse Public License (EPLv1)
- * http://waffle.codeplex.com/license
+/* Encodes and decodes to and from Base64 notation.
+ * Copyright (C) 2003 "Eric Glass" <jcifs at samba dot org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package waffle.util;
 
 /**
  * Base64 encoder/decoder. 
- * from https://spnego.svn.sourceforge.net/svnroot/spnego/trunk/src/java/net/sourceforge/spnego/Base64.java 
+ * from http://jcifs.samba.org/src/src/jcifs/util/Base64.java
  */
 public abstract class Base64 {
-
+	
     private static final String ALPHABET =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
