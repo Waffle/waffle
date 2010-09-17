27767c2f8fcf29ff79f3c4fd9071731c721c8e25
dblock <none@none.com>
2010-09-17 14:31:57 +0000

    Removed unused import. Fixed copyright notice - the code was not copied from an Apache product, but written to spec.

--- Source/JNAWindowsAuthProvider/src/waffle/spring/NegotiateSecurityFilter.java
+++ Source/JNAWindowsAuthProvider/src/waffle/spring/NegotiateSecurityFilter.java
@@ -1,17 +1,8 @@
 /*
- * Copyright 2002-2008 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (c) Application Security Inc., 2010
+ * All Rights Reserved
+ * Eclipse Public License (EPLv1)
+ * http://waffle.codeplex.com/license
  */
 
 package waffle.spring;
@@ -27,7 +18,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
