406185f0f749eb3888ce9df451c42f50ec3cbdfe
dblock <none@none.com>
2010-11-21 14:47:46 +0000

    Fixed x64 build, upgraded Jacob to 1.15M4 and JacobGen to 0.10. http://waffle.codeplex.com/workitem/9895

--- Tools/JacobGen/jacobgen.bat
+++ Tools/JacobGen/jacobgen.bat
@@ -1,22 +1,16 @@
 @echo off
 setlocal
 
-REM run this from the root directory of the Jacobgen project
-REM it will spit out the interface classes for a dll you pass in as a parameter
-REM sample command line while sitting in the JACOBGEN project directory
-REM
-REM The following command built a sample in the jacob directory I have 
-REM installed near my jacobgen project directory.
-REM $ docs/run_jacobgen.bat -destdir:"..\jacob\samples" -listfile:"jacobgenlog.txt" -package:com.jacobgen.microsoft.msword "C:\Program Files\Microsoft Office\OFFICE11\MSWORD.OLB"
-REM 
-REM
-
 set JRE=%JAVA_HOME%\bin\java.exe
-set JACOBGEN_HOME=.\
-set CLASSPATH=%CLASSPATH%;%JACOBGEN_HOME%jacobgen.jar;%JACOBGEN_HOME%lib\viztool.jar;%JACOBGEN_HOME%lib\samskivert.jar
+set JACOBGEN_HOME=%~dp0
 
-REM put the dll in the path where we can find it
-set PATH=%PATH%;%JACOBGEN_HOME%\x86
+set CLASSPATH=%JACOBGEN_HOME%jacobgen.jar;%JACOBGEN_HOME%lib\viztool.jar;%JACOBGEN_HOME%lib\samskivert.jar
+
+if NOT "%PROCESSOR_ARCHITEW6432%"=="" (
+ set PROCESSOR_ARCHITECTURE=%PROCESSOR_ARCHITEW6432%
+)
+
+set PATH=%JACOBGEN_HOME%\%PROCESSOR_ARCHITECTURE%;%PATH%
 
 "%JRE%" -Xint com.jacob.jacobgen.Jacobgen %1 %2 %3 %4 %5
 endlocal
