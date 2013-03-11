e4bf8d3f991b239a75e58f92092baf85f8e53cda
Dan Rollo <danrollo@gmail.com>
2013-03-11 17:04:14 -0400

    restore header

--- Source/JNA/waffle-shiro/src/waffle/shiro/dynamic/DynamicAuthenticationFilter.java
+++ Source/JNA/waffle-shiro/src/waffle/shiro/dynamic/DynamicAuthenticationFilter.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dan Rollo
+ *******************************************************************************/
+
 package waffle.shiro.dynamic;
 
 import waffle.shiro.negotiate.NegotiateAuthenticationFilter;
