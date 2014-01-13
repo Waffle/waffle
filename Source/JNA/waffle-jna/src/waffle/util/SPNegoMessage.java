e65181b5b48102a7534408d93785657e45604e4c
Ari Suutari <ari.suutari@syncrontech.com>
2014-01-13 13:55:18 +0200

    Detect also NegTokenArg packet. It doesn't have the same GSSAPI header in same way as NegTokenInit has, which caused that packet was passed to application instead of being processed by Waffle.

--- Source/JNA/waffle-jna/src/waffle/util/SPNegoMessage.java
+++ Source/JNA/waffle-jna/src/waffle/util/SPNegoMessage.java
@@ -23,9 +23,36 @@ public abstract class SPNegoMessage {
 	// SPNEGO OID, for details see http://msdn.microsoft.com/en-us/library/ms995330.aspx
 	private static final byte[]	spnegoOid	= {0x06, 0x06, 0x2b, 0x06, 0x01, 0x05, 0x05, 0x02};
 
-	public static boolean isSPNegoMessage(byte[] message) {
+	public static boolean isSPNegoMessage(byte[] message)
+	{
 		if (message == null || message.length < 2) {
 			return false;
+		}
+
+		int lenBytes;
+
+		// Check if this is NegTokenArg packet, it's id is 0xa1
+		if ((message[0] & 0xff) == 0xa1) {
+
+			int len;
+
+			// Get lenght of message for additional check.
+			if ((message[1] & 0x80) == 0)
+				len = message[1];
+			else {
+
+				lenBytes = message[1] & 0x7f;
+				len = 0;
+				int i = 2;
+				while (lenBytes > 0) {
+
+					len = len << 8;
+					len |= (message[i] & 0xff);
+					--lenBytes;
+				}
+			}
+
+			return (len + 2 == message.length);
 		}
 
 		// First byte should always be 0x60 (Application Constructed Object)
@@ -35,7 +62,7 @@ public abstract class SPNegoMessage {
 
 		// Next byte(s) contain token length, figure out
 		// how many bytes are used for length data
-		int lenBytes = 1;
+		lenBytes = 1;
 		if ((message[1] & 0x80) != 0) {
 			lenBytes = 1 + (message[1] & 0x7f);
 		}
@@ -43,7 +70,7 @@ public abstract class SPNegoMessage {
 		if (message.length < spnegoOid.length + 1 + lenBytes) {
 			return false;
 		}
-		
+
 		// Now check for SPNEGO OID, which should start just after length data.
 		for (int i = 0; i < spnegoOid.length; i++) {
 			if (spnegoOid[i] != message[i + 1 + lenBytes]) {
