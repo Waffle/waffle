b96e5f96162b9075790ecb9473bd39220113869f
Ari Suutari <ari.suutari@syncrontech.com>
2014-01-29 15:22:44 +0200

    Add unit test for NegTokenArg.

--- Source/JNA/waffle-jna/src-test/waffle/util/SPNegoMessageTests.java
+++ Source/JNA/waffle-jna/src-test/waffle/util/SPNegoMessageTests.java
@@ -29,6 +29,16 @@ public class SPNegoMessageTests {
 	
 	private static final byte[] negTokenInitOk = { 0x60, 0x76, 0x06, 0x06, 0x2B, 0x06, 0x01, 0x05, 0x05, 0x02  };
 	private static final byte[] negTokenInitTooShort = { 0x60, 0x76, 0x06, 0x06, 0x2B, 0x06, 0x01, 0x05, 0x05 };
+	
+	private static final byte[] negTokenArgOk = { (byte)0xA1, 0x33, 0x30, 0x31, 0x0, 0x03, 0x0A, 0x01, 
+												  0x01, 0x0, 0x2A, 0x04, 0x28, 0x4E, 0x54, 0x4C, 0x4D, 
+												  0x53, 0x53, 0x50, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0, 
+												  0x0, 0x08, 0x0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+												  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+												  0x00, 0x06, 0x01, 0x0, 0x1D, 0x00, 0x00, 0x00, 0x0F };
+	
+	private static final byte[] negTokenArgTooShort = { (byte)0xA1, 0x33, 0x30, 0x31, 0x0, 0x03 };
+	
 	private static final byte[] badMessage = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	
 	@Test
@@ -36,6 +46,8 @@ public class SPNegoMessageTests {
 		assertFalse(SPNegoMessage.isSPNegoMessage(null));
 		assertTrue(SPNegoMessage.isSPNegoMessage(negTokenInitOk));
 		assertFalse(SPNegoMessage.isSPNegoMessage(negTokenInitTooShort));
+		assertTrue(SPNegoMessage.isSPNegoMessage(negTokenArgOk));
+		assertFalse(SPNegoMessage.isSPNegoMessage(negTokenArgTooShort));
 		assertFalse(SPNegoMessage.isSPNegoMessage(badMessage));
 	}
 
@@ -44,5 +56,12 @@ public class SPNegoMessageTests {
 		assertTrue(SPNegoMessage.isNegTokenInit(negTokenInitOk));
 		assertFalse(SPNegoMessage.isNegTokenInit(negTokenInitTooShort));
 		assertFalse(SPNegoMessage.isNegTokenInit(badMessage));
+	}
+
+	@Test
+	public void testIsNegTokenArg() {
+		assertTrue(SPNegoMessage.isNegTokenArg(negTokenArgOk));
+		assertFalse(SPNegoMessage.isNegTokenArg(negTokenArgTooShort));
+		assertFalse(SPNegoMessage.isNegTokenArg(badMessage));
 	}
 }
