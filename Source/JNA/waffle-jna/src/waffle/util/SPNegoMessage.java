59d9786a5133a75d9a89d08c73d2929d800abb4d
Ari Suutari <ari.suutari@syncrontech.com>
2014-01-29 14:56:45 +0200

    Document detection of different tokens better.

--- Source/JNA/waffle-jna/src/waffle/util/SPNegoMessage.java
+++ Source/JNA/waffle-jna/src/waffle/util/SPNegoMessage.java
@@ -20,16 +20,26 @@ package waffle.util;
  */
 public abstract class SPNegoMessage {
 
-	// SPNEGO OID, for details see http://msdn.microsoft.com/en-us/library/ms995330.aspx
-	private static final byte[]	spnegoOid	= {0x06, 0x06, 0x2b, 0x06, 0x01, 0x05, 0x05, 0x02};
-
+	// Check if this message is SPNEGO auhentication token. There
+	// are two token types, NegTokenInit and NegTokenArg. 
+	// For details and specification, see
+	// http://msdn.microsoft.com/en-us/library/ms995330.aspx
 	public static boolean isSPNegoMessage(byte[] message) {
+		
+		// Message should always contains at least some kind of
+		// id byte and length. If it is too short, it
+		// cannot be a SPNEGO message.
 		if (message == null || message.length < 2) {
 			return false;
 		}
 
+		// Message is SPNEGO message if it is either NegTokenInit or NegTokenArg.
 		return isNegTokenInit(message) || isNegTokenArg(message);
 	}
+
+	// Check for NegTokenInit. It has always a special oid ("spnegoOid"),
+	// which makes it rather easy to detect.
+	private static final byte[]	spnegoOid	= {0x06, 0x06, 0x2b, 0x06, 0x01, 0x05, 0x05, 0x02};
 
 	public static boolean isNegTokenInit(byte[] message)
 	{
@@ -59,6 +69,10 @@ public abstract class SPNegoMessage {
 		return true;
 	}
 
+	// Check for NegTokenArg. It doesn't have oid similar to NegTokenInit.
+	// Instead id has one-byte id (0xa1). Obviously this is not
+	// a great way to detect the message, so we check encoded
+	// message length against number of received message bytes.
 	public static boolean isNegTokenArg(byte[] message)
 	{
 		// Check if this is NegTokenArg packet, it's id is 0xa1
