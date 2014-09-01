9325adfb5961e724f0e3b5089dc99621348a8075
Jeremy Landis <jeremylandis@hotmail.com>
2014-09-01 17:01:41 -0400

    Updated Change Log
    Notes about mocking changes and minor cleanup.

--- Source/JNA/waffle-tomcat6/src/test/java/waffle/apache/catalina/SimpleHttpRequest.java
+++ Source/JNA/waffle-tomcat6/src/test/java/waffle/apache/catalina/SimpleHttpRequest.java
@@ -42,10 +42,10 @@ public class SimpleHttpRequest extends Request {
     private String                    requestURI;
     private String                    queryString;
     private String                    remoteUser;
-    private String                    method      = "GET";
-    private final Map<String, String> headers     = new HashMap<String, String>();
+    private String                    method     = "GET";
+    private final Map<String, String> headers    = new HashMap<String, String>();
 
-    private final Map<String, String> parameters  = new HashMap<String, String>();
+    private final Map<String, String> parameters = new HashMap<String, String>();
 
     private byte[]                    content;
 
