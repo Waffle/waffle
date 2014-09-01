3123ca75ce0bd9f2bf58b29879df44c05c21659e
Jeremy Landis <jeremylandis@hotmail.com>
2014-09-01 16:15:33 -0400

    Mocking Tomcat 6
    Mocking tomcat so it is clear as to what waffle is actually using for
    tests.

--- Source/JNA/waffle-tomcat6/src/test/java/waffle/apache/catalina/SimpleHttpRequest.java
+++ Source/JNA/waffle-tomcat6/src/test/java/waffle/apache/catalina/SimpleHttpRequest.java
@@ -20,160 +20,167 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.apache.catalina.connector.Request;
+import org.mockito.Mockito;
 
 /**
+ * Simple HTTP Request.
+ * 
  * @author dblock[at]dblock[dot]org
  */
 public class SimpleHttpRequest extends Request {
 
-    private static int          _remotePort_s;
-
-    private String              _requestURI;
-    private String              _queryString;
-    private String              _remoteUser;
-    private String              _method     = "GET";
-    private String              _remoteHost;
-    private String              _remoteAddr;
-    private int                 _remotePort = -1;
-    private Map<String, String> _headers    = new HashMap<String, String>();
-    private Map<String, String> _parameters = new HashMap<String, String>();
-    private byte[]              _content;
-    private HttpSession         _session    = new SimpleHttpSession();
-    private Principal           _principal;
-
-    public SimpleHttpRequest() {
-        super();
-        _remotePort = nextRemotePort();
-    }
+    private static int remotePortS;
 
     public synchronized static int nextRemotePort() {
-        return ++_remotePort_s;
+        return ++SimpleHttpRequest.remotePortS;
     }
 
     public synchronized static void resetRemotePort() {
-        _remotePort_s = 0;
+        SimpleHttpRequest.remotePortS = 0;
+    }
+
+    private String                    requestURI;
+    private String                    queryString;
+    private String                    remoteUser;
+    private String                    method      = "GET";
+    private final Map<String, String> headers     = new HashMap<String, String>();
+
+    private final Map<String, String> parameters  = new HashMap<String, String>();
+
+    private byte[]                    content;
+
+    private SimpleHttpSession         httpSession;
+
+    private Principal                 principal;
+
+    public SimpleHttpRequest() {
+        super();
+        this.httpSession = Mockito.mock(SimpleHttpSession.class, Mockito.CALLS_REAL_METHODS);
+        this.httpSession.setAttributes(new HashMap<String, Object>());
+        this.remotePort = SimpleHttpRequest.nextRemotePort();
     }
 
     @Override
-    public void addHeader(String headerName, String headerValue) {
-        _headers.put(headerName, headerValue);
+    public void addHeader(final String headerName, final String headerValue) {
+        this.headers.put(headerName, headerValue);
     }
 
-    @Override
-    public String getHeader(String headerName) {
-        return _headers.get(headerName);
-    }
-
-    @Override
-    public String getMethod() {
-        return _method;
+    public void addParameter(final String parameterName, final String parameterValue) {
+        this.parameters.put(parameterName, parameterValue);
     }
 
     @Override
     public int getContentLength() {
-        return _content == null ? -1 : _content.length;
+        return this.content == null ? -1 : this.content.length;
     }
 
     @Override
-    public int getRemotePort() {
-        return _remotePort;
+    public String getHeader(final String headerName) {
+        return this.headers.get(headerName);
     }
 
     @Override
-    public void setMethod(String methodName) {
-        _method = methodName;
+    public String getMethod() {
+        return this.method;
     }
 
     @Override
-    public void setContentLength(int length) {
-        _content = new byte[length];
-    }
-
-    public void setRemoteUser(String username) {
-        _remoteUser = username;
-    }
-
-    @Override
-    public String getRemoteUser() {
-        return _remoteUser;
-    }
-
-    @Override
-    public HttpSession getSession() {
-        return _session;
-    }
-
-    @Override
-    public HttpSession getSession(boolean create) {
-        if (_session == null && create) {
-            _session = new SimpleHttpSession();
-        }
-        return _session;
+    public String getParameter(final String parameterName) {
+        return this.parameters.get(parameterName);
     }
 
     @Override
     public String getQueryString() {
-        return _queryString;
+        return this.queryString;
     }
 
     @Override
-    public void setQueryString(String queryString) {
-        _queryString = queryString;
-        if (_queryString != null) {
-            for (String eachParameter : _queryString.split("[&]")) {
-                String[] pair = eachParameter.split("=");
-                String value = (pair.length == 2) ? pair[1] : "";
-                addParameter(pair[0], value);
+    public String getRemoteAddr() {
+        return this.remoteAddr;
+    }
+
+    @Override
+    public String getRemoteHost() {
+        return this.remoteHost;
+    }
+
+    @Override
+    public int getRemotePort() {
+        return this.remotePort;
+    }
+
+    @Override
+    public String getRemoteUser() {
+        return this.remoteUser;
+    }
+
+    @Override
+    public String getRequestURI() {
+        return this.requestURI;
+    }
+
+    @Override
+    public HttpSession getSession() {
+        return this.httpSession;
+    }
+
+    @Override
+    public HttpSession getSession(final boolean create) {
+        if (this.httpSession == null && create) {
+            this.httpSession = Mockito.mock(SimpleHttpSession.class, Mockito.CALLS_REAL_METHODS);
+            this.httpSession.setAttributes(new HashMap<String, Object>());
+        }
+        return this.httpSession;
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return this.principal;
+    }
+
+    @Override
+    public void setContentLength(final int length) {
+        this.content = new byte[length];
+    }
+
+    @Override
+    public void setMethod(final String value) {
+        this.method = value;
+    }
+
+    @Override
+    public void setQueryString(final String queryValue) {
+        this.queryString = queryValue;
+        if (this.queryString != null) {
+            for (final String eachParameter : this.queryString.split("[&]")) {
+                final String[] pair = eachParameter.split("=");
+                final String value = pair.length == 2 ? pair[1] : "";
+                this.addParameter(pair[0], value);
             }
         }
     }
 
     @Override
-    public void setRequestURI(String uri) {
-        _requestURI = uri;
+    public void setRemoteAddr(final String value) {
+        this.remoteAddr = value;
     }
 
     @Override
-    public String getRequestURI() {
-        return _requestURI;
+    public void setRemoteHost(final String value) {
+        this.remoteHost = value;
+    }
+
+    public void setRemoteUser(final String value) {
+        this.remoteUser = value;
     }
 
     @Override
-    public String getParameter(String parameterName) {
-        return _parameters.get(parameterName);
-    }
-
-    public void addParameter(String parameterName, String parameterValue) {
-        _parameters.put(parameterName, parameterValue);
+    public void setRequestURI(final String value) {
+        this.requestURI = value;
     }
 
     @Override
-    public String getRemoteHost() {
-        return _remoteHost;
-    }
-
-    @Override
-    public void setRemoteHost(String remoteHost) {
-        _remoteHost = remoteHost;
-    }
-
-    @Override
-    public String getRemoteAddr() {
-        return _remoteAddr;
-    }
-
-    @Override
-    public void setRemoteAddr(String remoteAddr) {
-        _remoteAddr = remoteAddr;
-    }
-
-    @Override
-    public Principal getUserPrincipal() {
-        return _principal;
-    }
-
-    @Override
-    public void setUserPrincipal(Principal principal) {
-        _principal = principal;
+    public void setUserPrincipal(final Principal value) {
+        this.principal = value;
     }
 }
