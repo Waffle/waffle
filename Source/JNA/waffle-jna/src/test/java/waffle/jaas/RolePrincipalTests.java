4fc8a6b5f6639bb7ac9692a5262263bd5318ee95
Jeremy Landis <jeremylandis@hotmail.com>
2014-09-02 00:11:12 -0400

    Additional test coverage
    Some additional test cases for waffle-jna.

--- Source/JNA/waffle-jna/src/test/java/waffle/jaas/RolePrincipalTests.java
+++ Source/JNA/waffle-jna/src/test/java/waffle/jaas/RolePrincipalTests.java
@@ -13,9 +13,6 @@
  */
 package waffle.jaas;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -23,6 +20,8 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -33,9 +32,27 @@ public class RolePrincipalTests {
 
     private RolePrincipal rolePrincipal;
 
+    @Test
+    public void equals_otherObject() {
+        Assert.assertNotEquals(this.rolePrincipal, new String());
+    }
+
+    @Test
+    public void equals_sameObject() {
+        Assert.assertEquals(this.rolePrincipal, this.rolePrincipal);
+    }
+
     @Before
     public void setUp() {
         this.rolePrincipal = new RolePrincipal("localhost\\Administrator");
+    }
+
+    @Test
+    public void testEquals_Symmetric() {
+        final RolePrincipal x = new RolePrincipal("localhost\\Administrator");
+        final RolePrincipal y = new RolePrincipal("localhost\\Administrator");
+        Assert.assertEquals(x, y);
+        Assert.assertEquals(x.hashCode(), y.hashCode());
     }
 
     @Test
@@ -45,14 +62,14 @@ public class RolePrincipalTests {
         final ObjectOutputStream oos = new ObjectOutputStream(out);
         oos.writeObject(this.rolePrincipal);
         oos.close();
-        assertTrue(out.toByteArray().length > 0);
+        Assertions.assertThat(out.toByteArray().length).isGreaterThan(0);
         // deserialize
         final InputStream in = new ByteArrayInputStream(out.toByteArray());
         final ObjectInputStream ois = new ObjectInputStream(in);
-        final Object o = ois.readObject();
-        final RolePrincipal copy = (RolePrincipal) o;
+        final RolePrincipal copy = (RolePrincipal) ois.readObject();
         // test
-        assertEquals(this.rolePrincipal, copy);
-        assertEquals(this.rolePrincipal.getName(), copy.getName());
+        Assert.assertEquals(this.rolePrincipal, copy);
+        Assert.assertEquals(this.rolePrincipal.getName(), copy.getName());
     }
+
 }
