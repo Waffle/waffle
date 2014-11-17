319e98b8b41b871839ee219137b7c4968ff311f6
Jeremy Landis <jeremylandis@hotmail.com>
2014-11-16 22:49:57 -0500

    Enhance some for/while loop object creation
    We shouldn't be recreating objects inside loops, define the object
    outside the loop.

--- Source/JNA/waffle-jna/src/main/java/waffle/servlet/spi/SecurityFilterProviderCollection.java
+++ Source/JNA/waffle-jna/src/main/java/waffle/servlet/spi/SecurityFilterProviderCollection.java
@@ -48,14 +48,16 @@ public class SecurityFilterProviderCollection {
 
     @SuppressWarnings("unchecked")
     public SecurityFilterProviderCollection(final String[] providerNames, final IWindowsAuthProvider auth) {
+        Class<SecurityFilterProvider> providerClass;
+        Constructor<SecurityFilterProvider> providerConstructor;
+        SecurityFilterProvider provider;
         for (String providerName : providerNames) {
             providerName = providerName.trim();
             LOGGER.info("loading '{}'", providerName);
             try {
-                Class<SecurityFilterProvider> providerClass = (Class<SecurityFilterProvider>) Class
-                        .forName(providerName);
-                Constructor<SecurityFilterProvider> c = providerClass.getConstructor(IWindowsAuthProvider.class);
-                SecurityFilterProvider provider = c.newInstance(auth);
+                providerClass = (Class<SecurityFilterProvider>) Class.forName(providerName);
+                providerConstructor = providerClass.getConstructor(IWindowsAuthProvider.class);
+                provider = providerConstructor.newInstance(auth);
                 this.providers.add(provider);
             } catch (final ClassNotFoundException e) {
                 LOGGER.error("error loading '{}': {}", providerName, e.getMessage());
@@ -121,7 +123,6 @@ public class SecurityFilterProviderCollection {
      */
     public IWindowsIdentity doFilter(final HttpServletRequest request, final HttpServletResponse response)
             throws IOException {
-
         final AuthorizationHeader authorizationHeader = new AuthorizationHeader(request);
         final SecurityFilterProvider provider = get(authorizationHeader.getSecurityPackage());
         if (provider == null) {
@@ -138,13 +139,11 @@ public class SecurityFilterProviderCollection {
      * @return True if authentication is required.
      */
     public boolean isPrincipalException(final HttpServletRequest request) {
-
         for (SecurityFilterProvider provider : this.providers) {
             if (provider.isPrincipalException(request)) {
                 return true;
             }
         }
-
         return false;
     }
 
