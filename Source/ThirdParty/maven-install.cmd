149a0917d05af5bc3980bc29d482f8f128d4ab47
Jeremy Landis <jeremylandis@hotmail.com>
2015-04-11 15:46:45 -0400

    Drop spring 4 pulled in due to not being in central as it now is.

--- Source/ThirdParty/maven-install.cmd
+++ Source/ThirdParty/maven-install.cmd
@@ -1,5 +1,4 @@
 @echo off
 pushd "%~dp0"
-call mvn install:install-file -Dfile=spring-security\spring-security-core-4.0.0.M2.jar -DgroupId=org.springframework.security -DartifactId=spring-security-core -Dversion=4.0.0.M2 -Dpackaging=jar
-call mvn install:install-file -Dfile=spring-security\spring-security-web-4.0.0.M2.jar -DgroupId=org.springframework.security -DartifactId=spring-security-web -Dversion=4.0.0.M2 -Dpackaging=jar
+:: example -> call mvn install:install-file -Dfile=spring-security\spring-security-core-4.0.0.M2.jar -DgroupId=org.springframework.security -DartifactId=spring-security-core -Dversion=4.0.0.M2 -Dpackaging=jar
 popd
