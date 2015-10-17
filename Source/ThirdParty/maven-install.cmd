39a94543e5823d5192523e0f5761b42a1d5818c5
Jeremy Landis <jeremylandis@hotmail.com>
2015-10-16 22:55:11 -0400

    Remove thirt party jar installation info.

--- Source/ThirdParty/maven-install.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd "%~dp0"
-:: example -> call mvn install:install-file -Dfile=spring-security\spring-security-core-4.0.0.M2.jar -DgroupId=org.springframework.security -DartifactId=spring-security-core -Dversion=4.0.0.M2 -Dpackaging=jar
-popd
