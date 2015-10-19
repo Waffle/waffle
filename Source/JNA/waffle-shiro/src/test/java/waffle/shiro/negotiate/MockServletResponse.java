61fe1c21088df0af2a6f48179feea016a4af33a0
Jeremy Landis <jeremylandis@hotmail.com>
2015-10-18 21:51:17 -0400

    [shiro] reworking tests getting closer to all jmockit

---
+++ Source/JNA/waffle-shiro/src/test/java/waffle/shiro/negotiate/MockServletResponse.java
@@ -0,0 +1,95 @@
+/**
+ * Waffle (https://github.com/dblock/waffle)
+ *
+ * Copyright (c) 2010 - 2015 Application Security, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Application Security, Inc.
+ */
+package waffle.shiro.negotiate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The Class MockServletResponse.
+ */
+public abstract class MockServletResponse implements HttpServletResponse {
+
+    /** The is flushed. */
+    boolean                   isFlushed;
+
+    /** The error code. */
+    int                       errorCode;
+
+    /** The headers. */
+    Map<String, String>       headers;
+
+    /** The headers added. */
+    Map<String, List<String>> headersAdded;
+
+    /** The sc. */
+    int                       sc;
+
+    /*
+     * (non-Javadoc)
+     * @see javax.servlet.http.HttpServletResponse#addHeader(java.lang.String, java.lang.String)
+     */
+    @Override
+    public void addHeader(final String name, final String value) {
+        if (this.headersAdded.containsKey(name)) {
+            this.headersAdded.get(name).add(value);
+            return;
+        }
+
+        final List<String> values = new ArrayList<>();
+        values.add(value);
+        this.headersAdded.put(name, values);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.servlet.ServletResponse#flushBuffer()
+     */
+    @Override
+    public void flushBuffer() throws IOException {
+        this.isFlushed = true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.servlet.http.HttpServletResponse#sendError(int)
+     */
+    @Override
+    public void sendError(final int sendError) throws IOException {
+        this.errorCode = sendError;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.servlet.http.HttpServletResponse#setHeader(java.lang.String, java.lang.String)
+     */
+    @Override
+    public void setHeader(final String name, final String value) {
+        this.headers.put(name, value);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.servlet.http.HttpServletResponse#setStatus(int)
+     */
+    @Override
+    public void setStatus(final int status) {
+        this.sc = status;
+    }
+
+}
--- Source/JNA/waffle-shiro/src/test/java/waffle/shiro/negotiate/NegotiateAuthenticationFilterTest.java
+++ Source/JNA/waffle-shiro/src/test/java/waffle/shiro/negotiate/NegotiateAuthenticationFilterTest.java
@@ -13,15 +13,9 @@
  */
 package waffle.shiro.negotiate;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
-
-import mockit.Deencapsulation;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,92 +25,20 @@ import org.mockito.Mockito;
 import com.google.common.base.Joiner;
 import com.google.common.io.BaseEncoding;
 
+import mockit.Deencapsulation;
+import mockit.Tested;
+
 /**
  * The Class NegotiateAuthenticationFilterTest.
- *
- * @author Dan Rollo Date: 2/14/13 Time: 11:11 PM
  */
 public final class NegotiateAuthenticationFilterTest {
 
-    /**
-     * The Class MockServletResponse.
-     */
-    private static abstract class MockServletResponse implements HttpServletResponse {
-
-        /** The is flushed. */
-        boolean                         isFlushed;
-
-        /** The error code. */
-        int                             errorCode;
-
-        /** The headers. */
-        final Map<String, String>       headers      = new HashMap<>();
-
-        /** The headers added. */
-        final Map<String, List<String>> headersAdded = new HashMap<>();
-
-        /** The sc. */
-        int                             sc;
-
-        /*
-         * (non-Javadoc)
-         * @see javax.servlet.http.HttpServletResponse#addHeader(java.lang.String, java.lang.String)
-         */
-        @Override
-        public void addHeader(final String name, final String value) {
-            if (this.headersAdded.containsKey(name)) {
-                this.headersAdded.get(name).add(value);
-                return;
-            }
-
-            final List<String> values = new ArrayList<>();
-            values.add(value);
-            this.headersAdded.put(name, values);
-        }
-
-        /*
-         * (non-Javadoc)
-         * @see javax.servlet.ServletResponse#flushBuffer()
-         */
-        @Override
-        public void flushBuffer() throws IOException {
-            this.isFlushed = true;
-        }
-
-        /*
-         * (non-Javadoc)
-         * @see javax.servlet.http.HttpServletResponse#sendError(int)
-         */
-        @Override
-        public void sendError(final int sendError) throws IOException {
-            this.errorCode = sendError;
-        }
-
-        /*
-         * (non-Javadoc)
-         * @see javax.servlet.http.HttpServletResponse#setHeader(java.lang.String, java.lang.String)
-         */
-        @Override
-        public void setHeader(final String name, final String value) {
-            this.headers.put(name, value);
-        }
-
-        /*
-         * (non-Javadoc)
-         * @see javax.servlet.http.HttpServletResponse#setStatus(int)
-         */
-        @Override
-        public void setStatus(final int status) {
-            this.sc = status;
-        }
-
-    }
-
     /** The neg auth filter. */
+    @Tested
     private NegotiateAuthenticationFilter negAuthFilter;
 
     /** The response. */
-    private MockServletResponse           response;
+    MockServletResponse                   response;
 
     /** The out. */
     private byte[]                        out;
@@ -126,11 +48,9 @@ public final class NegotiateAuthenticationFilterTest {
      */
     @Before
     public void setUp() {
-        this.negAuthFilter = new NegotiateAuthenticationFilter();
-
         this.response = Mockito.mock(MockServletResponse.class, Mockito.CALLS_REAL_METHODS);
-        Deencapsulation.setField(this.response, "headers", new HashMap<String, String>());
-        Deencapsulation.setField(this.response, "headersAdded", new HashMap<String, String>());
+        Deencapsulation.setField(this.response, "headers", new HashMap<>());
+        Deencapsulation.setField(this.response, "headersAdded", new HashMap<>());
     }
 
     /**
