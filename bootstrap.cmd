047c369fb303ad979d03141f597d1f9431e06f9e
Jeremy Landis <jeremylandis@hotmail.com>
2015-11-01 20:55:48 -0500

    Added wix to bootstrap

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,3 +1,4 @@
 @echo off
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
 NuGet.exe install NUnit.Runners -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
+NuGet.exe install Wix.Toolset -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
