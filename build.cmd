19fcc5b3dd366adf833a964ae09998affd0db833
Jeremy Landis <jeremylandis@hotmail.com>
2015-11-01 22:34:16 -0500

    Change build.cmd to point to new location for nunit

--- build.cmd
+++ build.cmd
@@ -16,11 +16,9 @@ if NOT "%ProgramFiles(x86)%"=="" set ProgramFilesDir=%ProgramFiles(x86)%
 set VisualStudioCmd=%ProgramFilesDir%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
 if EXIST "%VisualStudioCmd%" call "%VisualStudioCmd%"
 
-for /D %%n in ( "%ProgramFilesDir%\NUnit*" ) do (
- set NUnitDir=%%~n
-)
+set NUnitDir=Tools\NUnit.Runners
 
-if EXIST "%NUnitDir%\bin" set NUnitBinDir=%NUnitDir%\bin
+if EXIST "%NUnitDir%\tools" set NUnitBinDir=%NUnitDir%\tools
 
 if NOT EXIST "%NUnitBinDir%" echo Missing NUnit, expected in %NUnitDir%
 if NOT EXIST "%NUnitBinDir%" exit /b -1
