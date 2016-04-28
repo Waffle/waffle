1628fab263037e1a1c6952da2e60bf63db9a94b5
Jeremy Landis <jeremylandis@hotmail.com>
2016-04-27 20:44:10 -0400

    [wix] Update wix to 3.10

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -3,4 +3,4 @@ Rmdir .\Tools\ /s /q
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
 NuGet.exe install NUnit -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
 NuGet.exe install NUnit.Runners -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
-NuGet.exe install Wix.Toolset -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
+NuGet.exe install Wix.Toolset.2015 -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
