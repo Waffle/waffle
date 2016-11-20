68a79b8df2ee9a9dc4971003faaa781aa640b1ee
Jeremy Landis <jeremylandis@hotmail.com>
2016-11-19 22:31:33 -0500

    [nuget] Update wix to official (pre release) and downgrade nunit to 3.4.1

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,6 +1,6 @@
 @echo off
 Rmdir .\Tools\ /s /q
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
-NuGet.exe install NUnit -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
-NuGet.exe install NUnit.Runners -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
-NuGet.exe install Wix.Toolset.2015 -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
+NuGet.exe install NUnit -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive -Version 3.4.1
+NuGet.exe install NUnit.ConsoleRunner -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive -Version 3.4.1
+NuGet.exe install WiX -Pre -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
