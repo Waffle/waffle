7414c23192afe2682448d28119f9ac7e943dbdcb
Jeremy Landis <jeremylandis@hotmail.com>
2019-12-26 19:43:48 -0500

    wagon-git commit

---
+++ waffle-jna/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"waffle.jaas"},{"l":"waffle.servlet"},{"l":"waffle.servlet.spi"},{"l":"waffle.util"},{"l":"waffle.windows.auth"},{"l":"waffle.windows.auth.impl"}]
