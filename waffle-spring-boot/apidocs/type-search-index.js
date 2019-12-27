2e14ca810df8b45a6437720021443bf2e4bd8460
Jeremy Landis <jeremylandis@hotmail.com>
2019-12-26 20:31:14 -0500

    wagon-git commit

---
+++ waffle-spring-boot/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"waffle.spring.boot","l":"WaffleProperties.SingleSignOnProperties"},{"p":"waffle.spring.boot","l":"WaffleAutoConfiguration"},{"p":"waffle.spring.boot.starter","l":"WaffleDummy"},{"p":"waffle.spring.boot","l":"WaffleProperties"}]
