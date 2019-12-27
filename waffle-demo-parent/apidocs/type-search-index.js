f6827b2a0786bbb7987e1f4fd35b653e84a78c79
Jeremy Landis <jeremylandis@hotmail.com>
2019-12-26 19:54:31 -0500

    wagon-git commit

---
+++ waffle-demo-parent/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"waffle.spring.boot.demo","l":"Application"},{"p":"waffle.spring.boot.demo","l":"DemoController"},{"p":"waffle.spring.boot.demo","l":"SecurityConfig"}]
