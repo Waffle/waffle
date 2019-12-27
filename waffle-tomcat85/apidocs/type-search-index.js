7ba264c913f89c618216eaf6b1ed78233ad3626c
Jeremy Landis <jeremylandis@hotmail.com>
2019-12-26 21:23:15 -0500

    wagon-git commit

---
+++ waffle-tomcat85/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"waffle.apache","l":"GenericWindowsPrincipal"},{"p":"waffle.apache","l":"MixedAuthenticator"},{"p":"waffle.apache","l":"NegotiateAuthenticator"},{"p":"waffle.apache","l":"WindowsRealm"}]
