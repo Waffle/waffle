2af5ad16448b8df9fe21fb67a36378885984a83f
Jeremy Landis <jeremylandis@hotmail.com>
2020-01-08 23:24:25 -0500

    [fix] Switch lamba to method expression for easier readability

--- Source/JNA/waffle-demo/waffle-spring-boot-filter/src/main/java/waffle/spring/boot/demo/DemoController.java
+++ Source/JNA/waffle-demo/waffle-spring-boot-filter/src/main/java/waffle/spring/boot/demo/DemoController.java
@@ -1,7 +1,7 @@
 /**
  * Waffle (https://github.com/Waffle/waffle)
  *
- * Copyright (c) 2010-2017 Application Security, Inc.
+ * Copyright (c) 2010-2020 Application Security, Inc.
  *
  * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
  * Public License v1.0 which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ package waffle.spring.boot.demo;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class DemoController {
     @RequestMapping
     public String demo(Authentication auth) {
         return String.format("Hello, %s. You have authorities: %s", auth.getPrincipal(),
-                auth.getAuthorities().stream().map(a -> a.getAuthority()).collect(Collectors.joining(", ")));
+                auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", ")));
     }
 
 }
