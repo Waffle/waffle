9775c2d341f763ca3e24f137fa0ef447450a28e4
Jeremy Landis <jeremylandis@hotmail.com>
2020-09-06 15:43:11 -0400

    [cleanup] Pre-license change work to backport missed items first

--- Source/JNA/waffle-jna/src/main/java/waffle/util/cache/Cache.java
+++ Source/JNA/waffle-jna/src/main/java/waffle/util/cache/Cache.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Waffle (https://github.com/Waffle/waffle)
  *
  * Copyright (c) 2010-2020 Application Security, Inc.
@@ -23,7 +23,8 @@ import java.util.ServiceLoader;
  *            the type of mapped values
  *
  * @author Simon Legner
- * @see <a href="https://github.com/Waffle/waffle/blob/master/Docs/CustomCache.md">Can I provide a custom cache implementation?</a>
+ * @see <a href="https://github.com/Waffle/waffle/blob/master/Docs/CustomCache.md">Can I provide a custom cache
+ *      implementation?</a>
  */
 public interface Cache<K, V> {
 
