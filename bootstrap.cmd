a68fa819884c792eef6d73cd693dc268ed876016
Jeremy Landis <jeremylandis@hotmail.com>
2020-11-25 17:59:56 -0500

    [ci] Remove trailing space in bootstrap.cmd

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -3,4 +3,4 @@ Rmdir .nuget\packages\ /s /q
 NuGet.exe install MSBuildTasks        -OutputDirectory .nuget\packages\ -NonInteractive -Version 1.5.0.214
 NuGet.exe install NUnit               -OutputDirectory .nuget\packages\ -NonInteractive -Version 3.5.0
 NuGet.exe install NUnit.ConsoleRunner -OutputDirectory .nuget\packages\ -NonInteractive -Version 3.5.0
-NuGet.exe install WiX                 -OutputDirectory .nuget\packages\ -NonInteractive -Version 4.0.0.4506-pre -Pre 
+NuGet.exe install WiX                 -OutputDirectory .nuget\packages\ -NonInteractive -Version 4.0.0.4506-pre -Pre
