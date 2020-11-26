13f136f1e45b7b27ff2dd655de8dfcd710f78f3a
Jeremy Landis <jeremylandis@hotmail.com>
2020-11-25 21:50:10 -0500

    [travis] Remove unused coveralls from configuration
    Was being done locally and has not been run since 2014.  Since sonar gives us the same as it uses jacoco just as this does, just use sonar.

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -6,15 +6,14 @@ commit_message=$(git log --format=%B -n 1)
 echo "Current commit detected: ${commit_message}"
 
 # We build for several JDKs on Travis.
-# Some actions, like analyzing the code (Coveralls) and uploading
-# artifacts on a Maven repository, should only be made for one version.
+# Some actions, such as uploading artifacts on a Maven repository,
+# should only be made for one version.
  
 # If the version is 1.8, then perform the following actions.
 # 1. Upload artifacts to Sonatype.
 # 2. Use -q option to only display Maven errors and warnings.
 # 3. Use --settings to force the usage of our "settings.xml" file.
-# 4. Notify Coveralls.
-# 5. Deploy site
+# 4. Deploy site
 
 if [ $TRAVIS_REPO_SLUG == "Waffle/waffle" ] && [ $TRAVIS_PULL_REQUEST == "false" ] && [ $TRAVIS_BRANCH == "master" ] && [[ "$commit_message" != *"[maven-release-plugin]"* ]]; then
 
@@ -22,11 +21,6 @@ if [ $TRAVIS_REPO_SLUG == "Waffle/waffle" ] && [ $TRAVIS_PULL_REQUEST == "false"
     # Deploy to sonatype
     ./mvnw deploy -DskipTests -q --settings ./.mvn/settings.xml
     echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
-
-    # Deploy to coveralls
-    # Cannot run tests on linux
-    # ./mvnw clean test jacoco:report coveralls:report -q --settings ./.mvn/settings.xml
-    # echo -e "Successfully deployed Coveralls Report under Travis job ${TRAVIS_JOB_NUMBER}"
 
     # Deploy to site
     # Cannot currently run site this way
