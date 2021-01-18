5706cfa47ade58579df57ce01d453629504a51f2
Freek Eek <freek.eek@etion.co.za>
2021-01-18 11:37:35 +0200

    Removed the warnings on specific desrialization tests.

--- Source/JNA/waffle-jna/src/test/java/waffle/jaas/RolePrincipalTest.java
+++ Source/JNA/waffle-jna/src/test/java/waffle/jaas/RolePrincipalTest.java
@@ -90,6 +90,7 @@ class RolePrincipalTest {
      *             the class not found exception
      */
     @Test
+    @SuppressWarnings("BanSerializableRead")
     void testIsSerializable() throws IOException, ClassNotFoundException {
         // serialize
         final ByteArrayOutputStream out = new ByteArrayOutputStream();
