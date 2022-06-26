09a4a077a6bcf333ba88fb7a758b166b28f37b8e
Jeremy Landis <jeremylandis@hotmail.com>
2022-06-26 17:26:40 -0400

    [ci] Formatting

--- Source/JNA/waffle-jna/src/main/java/waffle/util/AuthorizationHeader.java
+++ Source/JNA/waffle-jna/src/main/java/waffle/util/AuthorizationHeader.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2010-2020 The Waffle Project Contributors: https://github.com/Waffle/waffle/graphs/contributors
+ * Copyright (c) 2010-2022 The Waffle Project Contributors: https://github.com/Waffle/waffle/graphs/contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
