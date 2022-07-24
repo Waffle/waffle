147bee0fca6a351f8f56ed3d8c3e34d683bc5f8c
Jeremy Landis <jeremylandis@hotmail.com>
2022-07-24 17:34:39 -0400

    Wagon: Adding site to repository

--- waffle-tomcat85/testapidocs/package-search-index.js
+++ waffle-tomcat85/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"waffle.apache"},{"l":"waffle.apache.catalina"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"waffle.apache"},{"l":"waffle.apache.catalina"}];updateSearchResults();
