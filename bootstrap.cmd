587f318a14e2d61f3ebd70d9b9ce4da540cb78fd
Jeremy Landis <jeremylandis@hotmail.com>
2024-03-15 21:35:04 -0400

    [boostrap] Change how wix is installed

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -3,4 +3,6 @@ Rmdir .nuget\packages\ /s /q
 NuGet.exe install MSBuildTasks        -OutputDirectory .nuget\packages\ -NonInteractive -Version 1.5.0.235
 NuGet.exe install NUnit               -OutputDirectory .nuget\packages\ -NonInteractive -Version 3.14.0
 NuGet.exe install NUnit.ConsoleRunner -OutputDirectory .nuget\packages\ -NonInteractive -Version 3.17.0
-NuGet.exe install WiX                 -OutputDirectory .nuget\packages\ -NonInteractive -Version 4.0.4
+
+dotnet new tool-manifest
+dotnet tool install --global wix --version 4.0.4
