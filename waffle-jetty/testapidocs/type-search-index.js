c60888a4a2e27c9b5dd18e2e9c12edc46d51e741
Jeremy Landis <jeremylandis@hotmail.com>
2025-01-01 17:02:57 -0500

    Revert "Wagon: Adding site to repository"
    This reverts commit 3c0ffa9d502edd1869e0496caff31f8fa6b911b2.

--- waffle-jetty/testapidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"waffle.jetty","l":"StartEmbeddedJettyValidateNTLMGroup.InfoServlet"},{"p":"waffle.jetty","l":"StartEmbeddedJetty"},{"p":"waffle.jetty","l":"StartEmbeddedJettyValidateNTLMGroup"}];updateSearchResults();
