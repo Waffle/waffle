3c0ffa9d502edd1869e0496caff31f8fa6b911b2
Jeremy Landis <jeremylandis@hotmail.com>
2025-01-01 01:26:50 -0500

    Wagon: Adding site to repository

---
+++ waffle-jetty/testapidocs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"waffle.jetty"}];updateSearchResults();
---
+++ waffle-jetty/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"waffle.jetty","l":"waffle.jetty"}];updateSearchResults();
