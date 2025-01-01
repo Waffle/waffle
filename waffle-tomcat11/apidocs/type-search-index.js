48e77b94c433b9eea489bd37ac32542a02a23424
Jeremy Landis <jeremylandis@hotmail.com>
2025-01-01 17:03:35 -0500

    Revert "Wagon: Adding site to repository"
    This reverts commit 4921f0ca8fb11eec5a9bb5f9df4327750d1cd742.

--- waffle-tomcat11/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"waffle.apache","l":"GenericWindowsPrincipal"},{"p":"waffle.apache","l":"MixedAuthenticator"},{"p":"waffle.apache","l":"NegotiateAuthenticator"},{"p":"waffle.apache","l":"WindowsRealm"}];updateSearchResults();
