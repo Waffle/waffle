41c4c1a9b5f4226443889acfa3120f5bb2483f89
Jeremy Landis <jeremylandis@hotmail.com>
2025-01-01 00:38:25 -0500

    Wagon: Adding site to repository

---
+++ waffle-tomcat9/apidocs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"waffle.tomcat9"}];updateSearchResults();
---
+++ waffle-tomcat9/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"waffle.tomcat9","l":"waffle.apache"}];updateSearchResults();
