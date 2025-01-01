ddf8daf8a5cd9d08b578c3b3a6817c796cb3f7f6
Jeremy Landis <jeremylandis@hotmail.com>
2025-01-01 01:07:33 -0500

    Wagon: Adding site to repository

---
+++ waffle-tests-jakarta/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"waffle.tests.jakarta","l":"waffle.mock"},{"m":"waffle.tests.jakarta","l":"waffle.mock.http"}];updateSearchResults();
