c4c82688a3b3f81ba590dd4367e413b51021c25d
Jeremy Landis <jeremylandis@hotmail.com>
2026-09-12 21:15:22 -0400

    [dotnet] Replace 'MSBuildTasks' with modernized processing

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,7 +1,6 @@
 @echo off
 if exist .nuget\packages\ rmdir .nuget\packages\ /s /q
 
-NuGet.exe install MSBuildTasks        -OutputDirectory .nuget\packages\ -NonInteractive -Version 1.5.0.235
 NuGet.exe install NUnit               -OutputDirectory .nuget\packages\ -NonInteractive -Version 4.6.1
 NuGet.exe install NUnit.ConsoleRunner -OutputDirectory .nuget\packages\ -NonInteractive -Version 3.22.0
 
